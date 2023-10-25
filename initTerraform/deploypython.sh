373a2e3c32882384e1e27f4b6408df6f92f306af
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-10-25 14:48:20 -0400

    Add files via upload

---
+++ initTerraform/deploypython.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+sudo apt update
+
+sudo apt install -y software-properties-common 
+
+sudo add-apt-repository -y ppa:deadsnakes/ppa 
+
+sudo apt install -y python3.7 
+
+sudo apt install -y python3.7-venv
+
+sudo apt install -y build-essential
+
+sudo apt install -y libmysqlclient-dev
+
+sudo apt install -y python3.7-dev
+
+sudo apt update
+
+python3.7 -m venv temp
+
+source temp/bin/activate
+
+pip install mysqlclient
+
+pip install gunicorn
+
+deactivate
+
