10506a2aa895fed86d9f06beffd89fc82ca93584
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-10-25 16:20:08 -0400

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -2,16 +2,16 @@
 provider "aws" {
   alias = "us-east-1"  
   region =  "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_access_key
+  access_key = AWS_ACCESS_KEY
+  secret_key = AWS_SECRET_KEY
 
 }
 
 provider "aws" {
   alias = "us-west-2"
   region =  "us-west-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_access_key
+  access_key = AWS_ACCESS_KEY
+  secret_key = AWS_SECRET_KEY
 }
 
 #creating a VPC for east
